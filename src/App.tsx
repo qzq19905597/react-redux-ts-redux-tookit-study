cee4bb711184fc66220457950cf0b2cac3a4abc4
qiaozhiqiang <qiaozhiqiang@xiaomi.com>
2021-09-14 19:02:57 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <span>88888</span>
+        </div>
       </header>
     </div>
   );
